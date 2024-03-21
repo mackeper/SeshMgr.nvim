034cf9d7ec8c539ebea3c877c94b3feff3dba45d
Marcus Östling <mpt.ostling@gmail.com>
2024-03-21 20:47:26 +0100

    Add --noplugin

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 cd "$SCRIPT_DIR/.."
-nvim -u "$SCRIPT_DIR/minimal_init.vim" --headless -c ':lua require("mini.doc").generate(nil, "doc/SeshMgr.txt", nil)' -c 'q'
+nvim -u "$SCRIPT_DIR/minimal_init.vim" --headless --noplugin -c ':lua require("mini.doc").generate(nil, "doc/SeshMgr.txt", nil)' -c 'q'

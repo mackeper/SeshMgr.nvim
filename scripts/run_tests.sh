8ee315bc88b7d5b5ef6abf15c96d0c02c58b490d
Marcus Östling <mpt.ostling@gmail.com>
2024-03-21 21:02:44 +0100

    Add minimal_init for plenary

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 cd "$SCRIPT_DIR/.."
-nvim -u NORC --headless --noplugin -c ':lua require("plenary")' -c 'PlenaryBustedDirectory lua/tests'
+nvim -u "$SCRIPT_DIR/minimal_init.vim" --headless --noplugin -c ':lua require("plenary")' -c 'PlenaryBustedDirectory lua/tests'

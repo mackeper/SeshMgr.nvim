67fd41286eeb5294c8e468aec504f4b889c3f7cf
Marcus Östling <mpt.ostling@gmail.com>
2024-03-21 15:42:26 +0100

    Add a GitHub Actions workflow for checking the documentation (#5)
    * Add a GitHub Actions workflow for checking the documentation
    
    * Add echo debug messages to the docs.yml workflow
    
    * Change generation of docs to not use minimal_init.lua
    
    * Test a new minimal_init approach
    
    * Fix the minimal_init2.lua script
    
    * use minimal_init.lua instead of minimal_init2.lua
    
    * print docs in the workflow
    
    * Make minimal_init2.vim a vim script instead of a lua script
    
    * disable caching
    
    * Add a few debug lines
    
    * Fix minor error
    
    * More prints
    
    * Set output file name in generate_docs.sh
    
    * Fix the case of the SeshMgr.txt file in the docs.yml workflow
    
    * Make an error in SeshMgr.txt
    
    * Add exit code check to docs.yml
    
    * Clean up the workflow files and remove the debug output

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 cd "$SCRIPT_DIR/.."
-nvim -u "$SCRIPT_DIR/minimal_init.lua" --headless -c ':lua require("mini.doc").generate()' -c 'q'
+nvim -u "$SCRIPT_DIR/minimal_init.lua" --headless -c ':lua require("mini.doc").generate(nil, "doc/SeshMgr.txt", nil)' -c 'q'
---
+++ scripts/minimal_init2.vim
@@ -0,0 +1,7 @@
+set runtimepath+=.
+set runtimepath+=../plenary.nvim
+set runtimepath+=../mini.doc
+
+
+runtime! plugin/plenary.vim
+runtime! lua/mini/doc.lua
